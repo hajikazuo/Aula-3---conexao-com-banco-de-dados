9c1f99133894214487bb70ce91c87b2080874735
curso.ads2 <curso.ads2@LAB06-32>
2023-03-01 21:54:19 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 3 - conexao com banco de dados/Conexao.cs
@@ -0,0 +1,69 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_3___conexao_com_banco_de_dados
+{
+    internal class Conexao
+    {
+        static private string servidor = "";
+        static private string banco = "bd_aula3";
+        static private string usuario = "root";
+        static private string senha = "unifaat";
+
+        static public string strConn = $"server={servidor};" +
+            $"database={banco}; User Id={usuario};" +
+            $"password={senha}";
+
+        MySqlConnection cn;
+
+        private bool Conectar()
+        {
+            bool result;
+            try
+            {
+                cn = new MySqlConnection(strConn);
+                cn.Open();
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+            return result;
+        }
+        private void Desconectar()
+        {
+            if (cn.State == System.Data.ConnectionState.Open)
+            {
+                cn.Close();
+            }
+        }
+       
+        public bool Executar(String sql)
+        {
+            bool resultado=false;
+            if (Conectar())
+            {
+                try
+                {
+                    MySqlCommand cmd = new MySqlCommand(sql, cn);
+                    cmd.ExecuteNonQuery();
+                    resultado = true;
+                }
+                catch
+                {
+                    resultado = false;
+                }
+                finally
+                { 
+                    Desconectar();
+                }
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Aula 3 - conexao com banco de dados/TelaCliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula_3___conexao_com_banco_de_dados
+{
+    public partial class TelaCliente : Form
+    {
+        public TelaCliente()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
